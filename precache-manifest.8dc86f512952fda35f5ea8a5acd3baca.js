729c7ab1f46c68d46f5907f427f4d6ce86efde7d
grigoryanvahan <52545040+grigoryanvahan@users.noreply.github.com>
2019-08-08 23:43:08 +0400

    Updates

---
+++ precache-manifest.8dc86f512952fda35f5ea8a5acd3baca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8370aeb5da8ed5c335a2933e90535e5",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "60c852e014bb3158578c",
+    "url": "/weatherapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "50032256026a4a5fa70c",
+    "url": "/weatherapp/static/css/main.913bb6a0.chunk.css"
+  },
+  {
+    "revision": "60c852e014bb3158578c",
+    "url": "/weatherapp/static/js/2.40a8b625.chunk.js"
+  },
+  {
+    "revision": "50032256026a4a5fa70c",
+    "url": "/weatherapp/static/js/main.17c45c51.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  }
+]);
